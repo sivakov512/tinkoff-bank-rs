50f87048e06205541bee8241a4677b88987f75fc
Nikita Sivakov <sivakov512@gmail.com>
2021-03-26 22:42:50 +0300

    Update rust tooling

--- shell.nix
+++ shell.nix
@@ -1,14 +1,13 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { overlays = [ (import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz)) ]; } }:
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
     # needed to compile openssl-sys crate
     pkg-config
     openssl
-    cargo
-    clippy
-    rust-analyzer
-    rustc
-    rustfmt
+
+    (latest.rustChannels.stable.rust.override {
+      extensions = ["rust-src"];
+    })
   ];
 }
